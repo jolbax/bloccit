1d41dd44a847e218f11c3e9e99a5e7954c51d2f9
Jose Luis Barahona <jolubax@icloud.com>
2019-02-25 08:38:09 +0100

    Assignment-routing-controllers-view: Completed

--- spec/integration/static_spec.js
+++ spec/integration/static_spec.js
@@ -9,7 +9,17 @@ describe("route: static", () => {
         expect(res.statusCode).toBe(200);
         expect(body).toContain("Welcome to Bloccit");
         done();
-      })
-    })
-  })
+      });
+    });
+  });
+
+  describe("GET /about", () => {
+    it("should return status code 200 and have 'About Us' in the body of the response", (done) => {
+      request.get(base + "about", (err, res, body) => {
+        expect(res.statusCode).toBe(200);
+        expect(body).toContain("About Us");
+        done();
+      });
+    });
+  });
 });
