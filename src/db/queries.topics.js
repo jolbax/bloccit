42cd268040a2a12712ceb8ee8aba8f67e84240fa
Jose Luis Barahona <jolubax@icloud.com>
2019-02-26 14:51:39 +0100

    replace deprecated Model.all with Model.findAll

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -2,7 +2,7 @@ const Topic = require("./models").Topic;
 
 module.exports = {
   getAllTopics(callback) {
-    return Topic.all()
+    return Topic.findAll()
       .then(topics => {
         callback(null, topics);
       })
