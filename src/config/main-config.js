90fadbb8a883aa23f2188d909133a752e6e461bc
Jose Luis Barahona <jolubax@icloud.com>
2019-02-28 09:59:29 +0100

    Checkpoint-topic-resource-2: complete

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -1,11 +1,13 @@
 require("dotenv").config();
 const path = require("path");
 const viewsFolder = path.join(__dirname, "..", "views");
+const bodyParse = require("body-parser");
 
 module.exports = {
   init(app, express) {
     app.set("views", viewsFolder);
     app.set("view engine", "ejs");
+    app.use(bodyParse.urlencoded({extended: true}));
     app.use(express.static(path.join(__dirname, "..", "assets")));
   }
 };
--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -9,5 +9,54 @@ module.exports = {
       .catch(err => {
         callback(err);
       });
+  },
+  addTopic(newTopic, callback) {
+    return Topic.create({
+      title: newTopic.title,
+      description: newTopic.description
+    })
+      .then(topic => {
+        callback(null, topic);
+      })
+      .catch(err => {
+        callback(err);
+      });
+  },
+  getTopic(id, callback) {
+    return Topic.findByPk(id)
+      .then(topic => {
+        callback(null, topic);
+      })
+      .catch(err => {
+        callback(err);
+      });
+  },
+  deleteTopic(id, callback) {
+    return Topic.destroy({
+      where: { id }
+    })
+      .then(topic => {
+        callback(null, topic);
+      })
+      .catch(err => {
+        callback(err);
+      });
+  },
+  updateTopic(id, updatedTopic, callback) {
+    return Topic.findByPk(id).then(topic => {
+      if (!topic) {
+        return callback("Topic not found");
+      }
+
+      topic.update(updatedTopic, {
+          fields: Object.keys(updatedTopic)
+        })
+        .then(topic => {
+          callback(null, topic);
+        })
+        .catch(err => {
+          callback(err);
+        });
+    });
   }
 };
